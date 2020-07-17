f2d293b0241914efb93112304c5552a14092326d
Dejan Fajfar <dejan@fajfar.com>
2020-07-17 23:36:55 +0200

    Added external version

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
 
+version_file = open('version')
+version = version_file.read().strip()
+
 setup(
-    name="wdc",
-    version="0.1",
+    name='wdc',
+    version=version,
     packages=['wdc'],
     entry_points={
-        "console_scripts": ['wdc = wdc.runner:cli']
+        'console_scripts': ['wdc = wdc.runner:cli']
     },
 
     author='Dejan Fajfar',
     author_email='dejan@fajfar.com',
-    url='https://github.com/dejanfajfar/wdc'
+    url='https://github.com/dejanfajfar/wdc',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Utilities',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+    ]
 )
